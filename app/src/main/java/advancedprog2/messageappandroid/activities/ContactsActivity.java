0bfc5ca22f9e734b122072127b9f1cf667767d60
ZeevBinnes <92650073+ZeevBinnes@users.noreply.github.com>
2022-06-23 06:21:01 +0300

    added contacts search

--- app/src/main/java/advancedprog2/messageappandroid/activities/ContactsActivity.java
+++ app/src/main/java/advancedprog2/messageappandroid/activities/ContactsActivity.java
@@ -8,18 +8,26 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import advancedprog2.messageappandroid.R;
 import advancedprog2.messageappandroid.adapters.ContactsListAdapter;
 import advancedprog2.messageappandroid.database_classes.AppViewModel;
 import advancedprog2.messageappandroid.database_classes.UserWithContacts;
+import advancedprog2.messageappandroid.entities.Contact;
 
 public class ContactsActivity extends AppCompatActivity {
 
     private AppViewModel appViewModel;
     private String username;
+    private List<Contact> allContacts = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +57,7 @@ public class ContactsActivity extends AppCompatActivity {
         });
 
 
+
         RecyclerView contactsListLayout = findViewById(R.id.contactsList);
         final ContactsListAdapter adapter = new ContactsListAdapter(this, username);
         contactsListLayout.setAdapter(adapter);
@@ -57,7 +66,23 @@ public class ContactsActivity extends AppCompatActivity {
         appViewModel.getContacts(username).observe(this, new Observer<UserWithContacts>() {
             @Override
             public void onChanged(UserWithContacts userWithContacts) {
+                allContacts = userWithContacts.contacts;
                 adapter.setContacts(userWithContacts.contacts);
+            }
+        });
+
+        EditText edSearch = findViewById(R.id.contacts_search);
+        edSearch.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                List<Contact> contactsToShow = new ArrayList<>();
+                for (Contact c : allContacts) {
+                    if (c.getName().contains(edSearch.getText().toString())) {
+                        contactsToShow.add(c);
+                    }
+                }
+                adapter.setContacts(contactsToShow);
+                return false;
             }
         });
 
